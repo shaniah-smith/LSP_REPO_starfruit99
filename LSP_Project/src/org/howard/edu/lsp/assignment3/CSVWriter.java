6cdb77893c8103f559d00cffee6d0a0e8e1db6d5
Shaniah Smith <167722836+shaniah-smith@users.noreply.github.com>
2025-09-16 15:23:59 -0400

    CSVWriter.java

--- LSP_Project/src/org/howard/edu/lsp/assignment3/CSVWriter.java
+++ LSP_Project/src/org/howard/edu/lsp/assignment3/CSVWriter.java
@@ -7,7 +7,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Utility class for writing transformed product data to a CSV file.
+ * <p>
+ * This writer outputs a header row followed by the transformed results of
+ * the ETL process. Each row corresponds to a {@link RowResult}, containing
+ * product ID, uppercase name, final price, category, and price range.
+ */
 public class CSVWriter {
+
+    /**
+     * Writes the list of transformed product rows to a CSV file.
+     * <p>
+     * The output file will include a header row with the following columns:
+     * <pre>
+     * ProductID,Name,Price,Category,PriceRange
+     * </pre>
+     *
+     * @param path    the path to the CSV file where results should be written
+     * @param results the list of {@link RowResult} objects to output
+     * @throws IOException if an I/O error occurs while creating or writing the file
+     */
     public static void writeProducts(Path path, List<RowResult> results) throws IOException {
         try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
             // Write header
@@ -24,3 +44,4 @@ public class CSVWriter {
         }
     }
 }
+
